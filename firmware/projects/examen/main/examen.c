345d539ac14cd52723ed4a1ca6ad0f90e3326b6b
agustinamont <agustina.montanana@ingenieria.uner.edu.ar>
2024-11-10 19:30:25 -0300

    Examen con correcciones

--- firmware/projects/examen/main/examen.c
+++ firmware/projects/examen/main/examen.c
@@ -35,24 +35,27 @@
 #include "buzzer.h"
 #include "uart_mcu.h"
 #include "analog_io_mcu.h"
+#include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
-/*! @brief Período de refresco para la lectura del sensor (en milisegundos). */
+/*! @brief Período de refresco para la lectura del sensor y el control de alarma de precaucion (en milisegundos). */
 #define CONFIG_PERIOD_500 500
+/*! @brief Período de refresco para el control de alarma de peligro (en milisegundos). */
+#define CONFIG_PERIOD_250 250
 /*! @brief Período de refresco para el control de LEDs (en milisegundos). */
-#define CONFIG_PERIOD_1000 10
-/*! @brief Período de refresco para el temporizador (en milisegundos). */
-#define REFRESCO_SEND_DATA 500
+#define CONFIG_PERIOD_100 100
+/*! @brief Período de refresco para el muestreo de conversión ADC (en microsegundos). */
+#define REFRESCO_CONVERSION_ADC 10000
 /*! @brief Variable auxiliar para activar o desactivar la medición y el control de LEDs. */
 bool activar = true;
 /*! @brief Almacena la distancia medida por el sensor en centímetros. */
 float distancia = 0.00;
 /*==================[internal data definition]===============================*/
 /*! @brief Variable para almacenar el resultado de la conversión ADC.*/
-uint16_t value1 = 0;
+uint16_t senial_aceleracion_x = 0;
 /*! @brief Variable para almacenar el resultado de la conversión ADC.*/
-uint16_t value2 = 0;
+uint16_t senial_aceleracion_y = 0;
 /*! @brief Variable para almacenar el resultado de la conversión ADC.*/
-uint16_t value3 = 0;
+uint16_t senial_aceleracion_z = 0;
 
 /*! @brief Handle para la tarea de sensado de distancia. */
 TaskHandle_t sensar_task_handle = NULL;
@@ -65,11 +68,10 @@ TaskHandle_t adc_conversion_task_handle = NULL;
 /*==================[internal functions declaration]=========================*/
 /**
  * @fn
- * @brief Manejador de interrupción del Temporizador. Activa las tareas de conversión ADC y envío de datos.
+ * @brief Manejador de interrupción del Temporizador. Activa la tarea de conversión ADC.
  * @param param No se usa.
  */
-void FuncTimerSendData(void* param){
-	vTaskNotifyGiveFromISR(send_data_task_handle, pdFALSE);
+void FuncTimerConversionADC(void* param){
 	vTaskNotifyGiveFromISR(adc_conversion_task_handle, pdFALSE);
 }
 
@@ -92,13 +94,13 @@ static void SensarTask(void *pvParameter){
 }
 
 /*!
- * @brief Tarea que controla los LEDs según la distancia medida.
+ * @brief Tarea que controla las alarmas según la distancia medida.
  *
- * Esta tarea enciende o apaga los LEDs dependiendo de la distancia almacenada
- * en la variable global `distancia`.
+ * Esta tarea enciende o apaga los LEDs y el buzzer dependiendo de la distancia almacenada
+ * en la variable global `distancia`. Tambien envia el mensaje correspondiente a la UART.
  * @param[in] pvParameter Puntero a los parámetros de la tarea.
  */
-static void LedsTask(void *pvParameter){
+static void AlarmasTask(void *pvParameter){
 	while(true){
 		printf("Leds\n");
 		if(activar){
@@ -108,63 +110,35 @@ static void LedsTask(void *pvParameter){
 				LedOff(LED_3);      //led rojo
 				GPIOOff(GPIO_0);
 			}	
-			else if((distancia<500) & (distancia>300)){
+			else if((distancia<500) && (distancia>300)){
 				LedOn(LED_1);
 				LedOn(LED_2);
 				LedOff(LED_3);
+				
 				GPIOOn(GPIO_0);
+				vTaskDelay(CONFIG_PERIOD_500 / portTICK_PERIOD_MS);
+				GPIOOff(GPIO_0); 
+				vTaskDelay(CONFIG_PERIOD_500 / portTICK_PERIOD_MS);
+
+				UartSendString(UART_CONNECTOR, "Precaución, vehículo cerca.\r\n");
 			}
 			else if(distancia<300){
 				LedOn(LED_1);
 				LedOn(LED_2);
 				LedOn(LED_3);
+				
 				GPIOOn(GPIO_0);
+				vTaskDelay(CONFIG_PERIOD_250 / portTICK_PERIOD_MS);
+				GPIOOff(GPIO_0); 
+				vTaskDelay(CONFIG_PERIOD_250 / portTICK_PERIOD_MS);
+
+				UartSendString(UART_CONNECTOR, "Peligro, vehículo cerca.\r\n");
 			}
 		}
 		else{
 			LedsOffAll();
 		}
-		vTaskDelay(CONFIG_PERIOD_00 / portTICK_PERIOD_MS);
-	}
-}
-
-/*!
- * @brief Tarea que controla el buzzer segun el estado del GPIO_0.
- *
- * Esta tarea enciende o apaga EL BUZZER dependiendo si el GPIO_0 esta en alto o en bajo.
- * Ademas, segun la distancia medida por el sensor, tiene dos delays posibles.
- *
- * @param[in] pvParameter Puntero a los parámetros de la tarea.
- */
-static void BuzzerTask(void *pvParameter){
-	while(true){
-		if(GPIORead(GPIO_0)){
-			BuzzerOn();
-			if((distancia>300) & ((distancia<500))){
-				vTaskDelay(CONFIG_PERIOD_1000 / portTICK_PERIOD_MS);
-			}
-			else if(distancia<300){
-				vTaskDelay(CONFIG_PERIOD_500 / portTICK_PERIOD_MS);
-			}    
-		}
-		vTaskDelay(CONFIG_PERIOD_500 / portTICK_PERIOD_MS);
-	}
-}
-
-/**
- * @fn
- * @brief Tarea de FreeRTOS que envía datos del ADC por UART.
- * @param pvParameter No se usa.
- */		
-static void SendData(void *pvParameter){
-	while(true){
-		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		if((distancia<500) & (distancia>300)){
-			UartSendString(UART_CONNECTOR, "Precaución, vehículo cerca.\r\n");
-		}
-		else if(distancia<300){
-			UartSendString(UART_CONNECTOR, "Peligro, vehículo cerca.\r\n");
-		}
+		vTaskDelay(CONFIG_PERIOD_100 / portTICK_PERIOD_MS);
 	}
 }
 
@@ -176,13 +150,16 @@ static void SendData(void *pvParameter){
 static void ADC_Conversion(void *pvParameter){
 	while(true){
 		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-		AnalogInputReadSingle(GPIO_20, &value1);
-		AnalogInputReadSingle(GPIO_21, &value2);
-		AnalogInputReadSingle(GPIO_22, &value3);
+		AnalogInputReadSingle(CH1, &senial_aceleracion_x);
+		AnalogInputReadSingle(CH2, &senial_aceleracion_y);
+		AnalogInputReadSingle(CH3, &senial_aceleracion_z);
 
-		//1200 equivale a 4G en mV
-		if(value1+value2+value3 > 1200){
-			UartSendString(UART_CONNECTOR, "Caida detectada.\r\n");
+		float aceleracion_x = (senial_aceleracion_x / 1000.0 - 1.65) / 0.3;
+		float aceleracion_y = (senial_aceleracion_y / 1000.0 - 1.65) / 0.3;
+		float aceleracion_z = (senial_aceleracion_z / 1000.0 - 1.65) / 0.3;
+
+		if ((aceleracion_x + aceleracion_y + aceleracion_z) > 4){
+			UartSendString(UART_CONNECTOR, "Caida detectada\r\n");
 		}   
 	}
 }
@@ -191,58 +168,59 @@ static void ADC_Conversion(void *pvParameter){
  * @brief Punto de entrada principal de la aplicación. Inicializa los periféricos y comienza las tareas.
  */
 void app_main(void){
+	LedsInit();
+	HcSr04Init(GPIO_3, GPIO_2);
+	GPIOInit(GPIO_0, GPIO_OUTPUT);
+
 	serial_config_t serial_port = {
 		.port = UART_CONNECTOR,
 		.baud_rate = 115200,
 		.func_p = NULL,
 		.param_p = NULL
 	};
-	/* Inicialización de timer send data */
-    timer_config_t timer_send_data = {
-        .timer = TIMER_A,
-        .period = REFRESCO_SEND_DATA,
-        .func_p = FuncTimerSendData,
-        .param_p = NULL};
 
-	analog_input_config_t config_ADC = {
-		.input = GPIO_20,
+	/* Inicialización de timer conversion_ADC */
+    timer_config_t timer_conversion_adc = {
+        .timer = TIMER_A,
+        .period = CONFIG_PERIOD_500,
+        .func_p = FuncTimerConversionADC,
+        .param_p = NULL
+	};
+
+	analog_input_config_t config_ADC_CH1 = {
+		.input = CH1,
 		.mode = ADC_SINGLE,
 		.func_p = NULL,
 		.param_p = NULL,
 		.sample_frec = 0
 	};
 	
-	analog_input_config_t config_ADC = {
-		.input = GPIO_21,
+	analog_input_config_t config_ADC_CH2 = {
+		.input = CH2,
 		.mode = ADC_SINGLE,
 		.func_p = NULL,
 		.param_p = NULL,
 		.sample_frec = 0
 	};
 
-	analog_input_config_t config_ADC = {
-		.input = GPIO_22,
+	analog_input_config_t config_ADC_CH3 = {
+		.input = CH3,
 		.mode = ADC_SINGLE,
 		.func_p = NULL,
 		.param_p = NULL,
 		.sample_frec = 0
 	};
 
-	LedsInit();
-	HcSr04Init(GPIO_3, GPIO_2);
-	GPIOInit(GPIO_0, GPIO_OUTPUT);
-	GPIOInit(GPIO_20, GPIO_INPUT);
-	GPIOInit(GPIO_21, GPIO_INPUT);
-	GPIOInit(GPIO_22, GPIO_INPUT);
-	BuzzerInit(GPIO_0);
 	UartInit(&serial_port);
-    TimerInit(&timer_send_data);
-	AnalogInputInit(&config_ADC);
+    TimerInit(&timer_conversion_adc);
+	AnalogInputInit(&config_ADC_CH1);
+	AnalogInputInit(&config_ADC_CH2);
+	AnalogInputInit(&config_ADC_CH3);
+
 	
 	xTaskCreate(&SensarTask, "Sensar", 1024, NULL, 4, &sensar_task_handle);
-	xTaskCreate(&LedsTask, "Leds", 1024, NULL, 4, &leds_task_handle);
-	xTaskCreate(&SendData, "Send Data", 2048, NULL, 4, &send_data_task_handle);
-	xTaskCreate(&ADC_Conversion, "ConversionADC", 2048, NULL, 4, &adc_conversion_task_handle);
+	xTaskCreate(&AlarmasTask, "Leds", 1024, NULL, 3, &leds_task_handle);
+	xTaskCreate(&ADC_Conversion, "ConversionADC", 2048, NULL, 2, &adc_conversion_task_handle);
 
 }
 /*==================[end of file]============================================*/
